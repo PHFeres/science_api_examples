d87a59ae6a96d64ab78779d0a8a784f826996292
Wenderson Souza <wenderson.souza@enacom.com.br>
2021-08-12 21:01:57 -0300

    feat: added scenario id to example io

--- science_api_examples/io/example_io.py
+++ science_api_examples/io/example_io.py
@@ -5,16 +5,13 @@ from science_api.io import IOBaseClass
 
 class ExampleIO(IOBaseClass):
 
-
-    def __init__(self, input_data):
-
+    def __init__(self, input_data, scenario_id='A0'):
         self.input_data = input_data
+        self.scenario_id = scenario_id
 
     def get_output(self) -> dict:
-
         responses = list()
         for elem in self.input_data["people"]:
-
             aux_currency = Currency[elem['unit'].upper()]
             responses.append(
                 f"hello {elem['name']}, you have {elem['money']} {aux_currency.value}."
